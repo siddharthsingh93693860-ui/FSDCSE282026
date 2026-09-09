d72a5f7143199f807cd43715f0a81d19a05c5a9e
siddharthsingh93693860-ui <siddharthsingh93693860@gmail.com>
2026-09-09 09:46:51 +0530

    manni

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 console.log("hello siddh")
+
+const btn = document.getElementById('btn');
+console.log(btn);
+
+const root = document.getElementById('root');
+console.log(root);
+
+const h1 = document.createElement('h1');
+
+const loader = document.createElement('h2');
+loader.innerHTML = "Loading.....";
+
+async function showData() {
+    try {
+        // Show loader before fetching data
+        root.appendChild(loader);
+
+        const serverData = await fetch("https://fakestoreapi.com/products");
+        const jsonData = await serverData.json();
+
+        // Heading
+        h1.innerHTML = `<h1 style="color:red">${jsonData[0].title}</h1>`;
+
+        // Create table
+        let table = `
+            <table border="1" cellpadding="10">
+                <tr>
+                    <th>Image</th>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                </tr>
+
+                ${jsonData.map((ele) => {
+                    return `
+                        <tr>
+                            <td>
+                                <img src="${ele.image}" height="200" width="200">
+                            </td>
+                            <td>${ele.id}</td>
+                            <td>${ele.title}</td>
+                            <td>${ele.price}</td>
+                        </tr>
+                    `;
+                }).join("")}
+
+            </table>
+        `;
+
+        root.appendChild(h1);
+
+        // Add table to webpage
+        root.innerHTML += table;
+
+    } catch (e) {
+        console.log(e);
+    } finally {
+        // Remove loader
+        if (root.contains(loader)) {
+            root.removeChild(loader);
+        }
+    }
+}
+
+btn.addEventListener('click', showData);
